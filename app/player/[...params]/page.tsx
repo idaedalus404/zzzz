bf97da6ab101834917f12d85048aadfe49ecffe3
jerometecson-main <zxctest8@zxcstream.xyz>
2026-07-20 05:24:07 +0800

    final restric pattern

--- app/player/[...params]/page.tsx
+++ app/player/[...params]/page.tsx
@@ -69,15 +69,15 @@ export default function Player() {
   const trackedRef = useRef(false);
   const utcHour = new Date().getUTCHours();
   const phHour = (utcHour + 8) % 24;
-  // const restrictionActive = phHour >= 20 || phHour < 8; // 8pm–8am PH
+  const restrictionActive = phHour >= 20 || phHour < 8; // 8pm–8am PH
 
-  const restrictionActive = phHour >= 17 || phHour < 5;
+  // const restrictionActive = phHour >= 17 || phHour < 5;
   const isPartner = document.referrer.includes("xullys.xyz");
   const restrictedSites = [
     "streamex",
     "nightflix",
     "zxcstream",
-    // "redflix",
+    "redflix",
     // "pantyflix",
     // "vibemax",
     // "cinevibe",
